002322502232da9e3e4800dd33764661a21610dd
CO722G1-NguyenDuyDung <duydungne10@gmail.com>
2022-09-14 21:07:15 +0700

    nop bai

--- src/ss7/execrise/execrise2/TextColorable.java
+++ src/ss7/execrise/execrise2/TextColorable.java
@@ -6,13 +6,13 @@ public class TextColorable {
     public static void main(String[] args) {
         Colorable[] colorable = new Colorable[3];
         colorable[0] = new Circle(10.0);
-        colorable[1] = new Ractangle(10.0,10.0);
-        colorable[2] =new Square(10);
-        for (Colorable colorable1 : colorable ) {
-            System.out.println("diện tích là "+ colorable1 );
-            if (colorable1 instanceof Square){
-                System.out.println();
+        colorable[1] = new Ractangle(10.0, 10.0);
+        colorable[2] = new Square(10);
+        for (Colorable colorable1 : colorable) {
+            System.out.println("diện tích là " + colorable1);
+            if (colorable1 instanceof Square) {
+                colorable1.howToColor();
             }
         }
-}
+    }
 }
